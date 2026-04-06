7fc6243302ff57dc6af879a06d96462b2d6d3363
joacomoreno-droid <joaquinmorenotdf@gmail.com>
2026-04-06 18:34:35 -0300

    Update price_calculator.py

--- price_calculator.py
+++ price_calculator.py
@@ -21,7 +21,10 @@ def final_price(price, quantity, discount_pct, tax_pct):
       3. Aplicar el impuesto al resultado usando apply_tax.
       4. Retornar el resultado redondeado a 2 decimales usando round().
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    subtotal = price * quantity
+    price_discount = apply_discount(subtotal, discount_pct)
+    price_tax = apply_tax(price_discount, tax_pct)
+    return round(price_tax, 2)
 
 def best_deal(price_a, qty_a, disc_a, price_b, qty_b, disc_b, tax_pct):
     """
@@ -31,4 +34,10 @@ def best_deal(price_a, qty_a, disc_a, price_b, qty_b, disc_b, tax_pct):
     Si son iguales, retorna "A".
     Debe USAR la función final_price para resolver el ejercicio.
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    final_a = final_price(price_a, qty_a, disc_a, tax_pct)
+    final_b = final_price(price_b,qty_b, disc_b, tax_pct)
+
+    if final_a <= final_b:
+        return "A"
+    else:
+        return "B"
